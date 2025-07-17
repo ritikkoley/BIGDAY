c8053cbdc525c8897919d74af6b2985fc5385205
ritikkoley <58953220+ritikkoley@users.noreply.github.com>
2025-07-17 14:49:04 +0530

    Updated StudentPortal.tsx

--- src/components/student/StudentPortal.tsx
+++ src/components/student/StudentPortal.tsx
@@ -2,21 +2,18 @@
  import { useDataStore } from '../../stores/dataStore';
  
  // Student Components 
--import { Home } from '../student/Home';
--import { StudentProgress } from '../student/StudentProgress';
-+import Home from '../student/Home';
-+import Progress from '../student/Progress';
+import Home from '../student/Home';
+import Progress from '../student/Progress';
  import { StudyVault } from '../StudyVault';
  import { GradesView } from '../GradesView';
  import { AttendanceView } from '../AttendanceView';
 @@ .. @@
-           {activeTab === 'home' && (
-             <Home />
-           )}
--          {activeTab === 'progress' && (
--            <StudentProgress />
-+          {activeTab === 'progress' && (
-+            <Progress />
-           )}
-           {activeTab === 'study-vault' && (
-             <StudyVault
+          {activeTab === 'home' && (
+            <Home />
+          )}
+          {activeTab === 'progress' && (
+            <Progress />
+          )}
+          {activeTab === 'study-vault' && (
+            <StudyVault />
+          )}
